c9c64386e287d5005d75000a7a1f6c4e56977831
LaetitiaDaCosta <laetitiadacosta@mailoo.org>
2019-03-15 14:40:56 +0100

    ajout template page last game

--- bottle_laetitia.py
+++ bottle_laetitia.py
@@ -1,12 +1,20 @@
 from bottle import route, run, template
-from models import StatsPerDay
+from models import StatsPerDay, StatsPerMatch
 
 
-
+# page stats par jour
 @route('/page_stats_day/<nom_machine>')
 def page_stats_day(nom_machine):
-    return template('page_stat_par_jour', nom=nom_machine, stats=StatsPerDay.select())
+    return template('page_stat_par_jour', nom=nom_machine, stats=StatsPerDay.select().order_by())
+
+
+# page affichage de la dernière partie jouée
+@route('/page_last_game')
+def page_last_game():
+    last_result = StatsPerMatch.select().order_by(StatsPerMatch.date_debut.desc())[0]
+    return template('page_last_game', last_game=last_result)
 
 
 if __name__ == '__main__':
     run(debug=True, reloader=True)
+
---
+++ page_last_game.tpl
@@ -0,0 +1,11 @@
+<h1>Ceci est la page qui affiche le résultat de la dernière partie</h1>
+
+
+
+{{last_game.machine.nom}}
+{{last_game.machine.jeu_installe}}
+{{last_game.duree_jeu}}
+{{last_game.gagnant}}
+
+   <br>
+
