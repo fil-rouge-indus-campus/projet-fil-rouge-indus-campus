26a83700c34f5c0d5812a266c47aa6cea917dd37
yoskama26 <robinpeyret26@gmail.com>
2019-03-15 11:34:51 +0100

    ajout de la page_stat_par_jour.tpl, du fichier bottle_laetitia.py et quelques modifs sur le fichier models.py

--- bottle-robin.py
+++ bottle-robin.py
@@ -9,7 +9,13 @@ def index():
 
 @route('/test/<oui>/<nom>')
 def test(oui, nom):
-    return '<h1> ceci est la page aaaaaaaaaa test numéro ' + oui + ' qui appartient a ' + nom + '</h1>'
+    return '<h1> ceci est la page test numéro ' + oui + ' qui appartient a ' + nom + '</h1>'
+
+
+@route('/lastresult')
+def lastresult():
+    last_result = StatsPerMatch.select().order_by(StatsPerMatch.date_debut.desc())[0]
+    return template('lastresulteuu', game=last_result)
 
 
 @route('/index')
